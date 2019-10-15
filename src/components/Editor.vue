d99ad64a89dd99b824f2892792fe11e0e6f60028
Rostislav Jadavan <rostislav@jadavan.com>
2019-10-15 19:13:47 +0200

    editor with github theme

---
+++ src/components/Editor.vue
@@ -0,0 +1,51 @@
+<template>
+    <section>
+        <div class="editor">
+            <textarea ref="editor" value></textarea>
+        </div>
+    </section>
+</template>
+
+<script>
+import CodeMirror from "codemirror/lib/codemirror.js";
+import "codemirror/lib/codemirror.css";
+
+export default {
+    props: {
+        content: String
+    },
+    data() {
+        return {        
+            instance: null,
+            options: {
+                value: "",
+                theme: "github-light"
+            }
+        };
+    },
+    mounted() {
+        this.init();
+    },
+    methods: {
+        init() {
+            this.instance = CodeMirror.fromTextArea(
+                this.$refs.editor,
+                this.options
+            );
+            this.instance.setValue(this.options.value || this.content);
+            this.instance.on("change", cm => {                
+                this.$emit("change", cm.getValue());
+            });
+        }
+    }
+};
+</script>
+
+
+<style>
+@import "../assets/github-codemirror-theme.css";
+
+.CodeMirror {
+    height: auto;
+}
+</style>
---
+++ src/components/Markdown.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="markdown-body" v-html="markdownOutput" />
+</template>
+
+<script>
+import Markdown from "markdown-it";
+
+export default {
+    props: {
+        content: String
+    },
+    data() {
+        return {
+            outputContent: "",
+            md: new Markdown({
+                html: true,
+                linkify: true,
+                typographer: true
+            })
+        };
+    },
+    computed: {
+        markdownOutput: function() {            
+            return this.md.render(this.content);
+        }
+    }    
+};
+</script>
